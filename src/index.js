9631e00cd28383e49cc906b7be44a61e8b6d48bb
andrewboles <andrewboles@me.com>
2021-12-06 14:38:11 -0600

    added project to item functionality, need to add todo list wipe with project switch, modal for todo item info, and local storage saving

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ const project = () => {
   let itemList = {};
   const addItem = (item) =>{
     itemList[item.getItem().title] = item
-    manipulateDOM.appendItem(item.getItem().title)
   }
   const removeItem = (item) =>{
     delete itemList[item.getItem().title];
@@ -51,6 +50,9 @@ const manipulateDOM = (() =>{
     addProjButton.addEventListener('click',()=>{
       manipulateDOM.appendProject()
     })
+    addItemButton.addEventListener('click',()=>{
+      manipulateDOM.appendItem()
+    })
     projectContainer.addProject()
 
 
@@ -61,59 +63,86 @@ const manipulateDOM = (() =>{
     let removeButton = document.createElement('div');
     let addProjButton = document.getElementById('add-project')
     let projectName = ""
+    newProjDom.addEventListener('click', e =>{
+      projectContainer.setProject(e.target.id)
+      let allEntries = document.querySelectorAll('.project-entry')
+      allEntries.forEach(e => {
+        e.classList.remove('project-entry-highlight')
+      })
+      newProjDom.classList.add('project-entry-highlight')
+    });
     sideBar.insertBefore(newProjDom, addProjButton)
 
     newProjDom.classList.add('project-entry');
     // newProjDom.innerHTML = `${projectName}`
-    newProjDom.innerHTML = `<form id = "project-name-form" onsubmit = "return false" >
-              <input type="text" id="title" name="title" ><br></form>`;
+    let nameForm = document.createElement('form');
+    nameForm.setAttribute('onsubmit','return false');
+    nameForm.innerHTML = '<input type="text" id="title" name="title" ><br></form>'
     removeButton.classList.add('remove-button');
     removeButton.innerHTML = `x`
     removeButton.addEventListener('click', e => {
-      removeProject(e.target.parentNode.id)
-    })
-    newProjDom.appendChild(removeButton)
-    let form = document.querySelector('#project-name-form')
-    form.addEventListener('submit',e =>{
-      projectName = form.elements['title'].value;
+      removeProject(e.target.parentNode)
+    });
+    newProjDom.appendChild(nameForm);
+    newProjDom.appendChild(removeButton);
+    
+    nameForm.addEventListener('submit',e =>{
+
+      projectName = nameForm.elements['title'].value;
+      newProjDom.setAttribute('id',`${projectName}`);
+      projectContainer.setProject(e.target.parentNode.id)
+      projectContainer.addProject(projectName)
       newProjDom.innerHTML = projectName;
       newProjDom.appendChild(removeButton)
-      newProjDom.setAttribute('id',`${projectName}-project`);
-      projectContainer.addProject(projectName)
     })
-    return projectName
     
   }
   const appendItem = (itemName) => {
-    let newItemDom = document.createElement('div');
-    let addItemButton = document.getElementById('add-item')
-    newItemDom.setAttribute('id',`${itemName}`);
-    newItemDom.classList.add('item-entry');
-    newItemDom.innerHTML = `${itemName}`
+    let newProjDom = document.createElement('div');
     let removeButton = document.createElement('div');
+    let addItemButton = document.getElementById('add-item')
+    let projectName = ""
+    newProjDom.addEventListener('click', e =>{
+      // projectContainer.setProject(e.target.id)
+      // newProjDom.classList.toggle('project-entry-highlight')
+    });
+    todolistspace.insertBefore(newProjDom, addItemButton)
+
+    newProjDom.classList.add('item-entry');
+    // newProjDom.innerHTML = `${projectName}`
+    let nameForm = document.createElement('form');
+    nameForm.setAttribute('onsubmit','return false');
+    nameForm.innerHTML = '<input type="text" id="title" name="title" ><br></form>'
     removeButton.classList.add('remove-button');
     removeButton.innerHTML = `x`
     removeButton.addEventListener('click', e => {
-      removeItem(e.target.parentNode.id)
+      removeItem(e.target.parentNode)
+    });
+    newProjDom.appendChild(nameForm);
+    newProjDom.appendChild(removeButton);
+    
+    nameForm.addEventListener('submit',e =>{
+      itemName = nameForm.elements['title'].value;
+      newProjDom.innerHTML = itemName;
+      newProjDom.appendChild(removeButton)
+      newProjDom.setAttribute('id',`${itemName}`);
+      projectContainer.projectList[`${projectContainer.getCurrentProject()}`].addItem(todoItem(`${itemName}`, "do your laundry", new Date(2020, 1, 11),'low'),`${projectContainer.getCurrentProject()}`)
     })
-    newItemDom.appendChild(removeButton)
-    todolistspace.insertBefore(newItemDom, addItemButton)
   }
 
-  const removeProject = (projectId) => {
-    let toRemoveDiv = document.getElementById(`${projectId}`)
-    if(toRemoveDiv === null){
+  const removeProject = (project) => {
+    
+    if(project === null){
       return
     }
-    sideBar.removeChild(toRemoveDiv)
+    sideBar.removeChild(project)
   }
 
-  const removeItem = (itemName) => {
-    let toRemoveDiv = document.getElementById(`${itemName}`)
-    if(toRemoveDiv === null){
+  const removeItem = (item) => {
+    if(item === null){
       return
     }
-    todolistspace.removeChild(toRemoveDiv)
+    todolistspace.removeChild(item)
   }
 
 
@@ -123,24 +152,23 @@ const manipulateDOM = (() =>{
 
 const projectContainer = (() => {
   let projectList = {};
+  let selected_project = null
   const addProject = (projectName) =>{
     projectList[projectName] = project()
   }
   const removeProject = (projectName) =>{
-    delete projectList[projectName]
-    manipulateDOM.removeProject(projectName)
+    manipulateDOM.removeProject(projectName);
+    delete projectList[projectName];
   }
 
-  return {projectList, addProject, removeProject};
+  const setProject = (projectName) => {
+    selected_project = projectName;
+  }
+
+  const getCurrentProject = () => selected_project
+
+  return {projectList, addProject, removeProject, selected_project, setProject, getCurrentProject};
 })();
 
 
 manipulateDOM.startUp()
-
-
-// projectContainer.projectList.defaultProject.addItem(todoItem("laundry", "do your laundry", new Date(2020, 1, 11),'low'))
-
-// projectContainer.projectList['defaultProject'].changeItemProject('laundry','home')
-
-
-
